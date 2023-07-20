3612e92f6ba5f8c65e7fa50bcd03a896264aa2ce
Matt Goodhart <matthewagoodhart@gmail.com>
2023-07-20 11:27:19 -0400

    Initial Commit

---
+++ GoL/GoLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoLApp.swift
+//  GoL
+//
+//  Created by Matthew Goodhart on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GoLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

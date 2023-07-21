4e5af651db8444259d746de9a75d40ffec56b8a7
Matt Goodhart <matthewagoodhart@gmail.com>
2023-07-21 13:35:52 -0400

    Implements basic UI

--- GoL/ContentView.swift
+++ GoL/ContentView.swift
@@ -7,15 +7,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct Square: View {
+    
+    let xPosition: Int
+    let yPosition: Int
+    @State var isAlive: Bool = false
+    
+    public func isNeighbor(to square: Square) -> Bool {
+
+        // using abolute value minimizes cases to test
+        let xDistance = abs(self.xPosition - square.xPosition)
+        let yDistance = abs(self.yPosition - square.yPosition)
+        
+        switch (xDistance, yDistance) {
+        case (1, 1), (0, 1), (1, 0):
+            return true
+        default:
+            return false
         }
-        .padding()
+    }
+    
+    var body: some View {
+        let screenWidth = UIScreen.main.bounds.width
+        var numberOfColumns = 10
+        
+        let squareWidth = screenWidth / CGFloat(numberOfColumns + 1)
+        Rectangle()
+            .frame(width: squareWidth, height: squareWidth, alignment: .center)
+            .foregroundColor(isAlive == false ? .gray : Color("MercuryLime") )
+            .border(.black)
+            .onTapGesture {
+                if self.isAlive == false {
+                    self.isAlive = true
+                } else {
+                    self.isAlive = false
+                }
+            }
+    }
+}
+
+struct GameGrid: View {
+
+    var rowsCount = 10
+    var columnsCount = 10
+    
+    var body: some View {
+        Grid(alignment: .topLeading, horizontalSpacing: 0, verticalSpacing: 0) {
+            ForEach(0..<rowsCount, id: \.self) { row in
+                GridRow(alignment: .firstTextBaseline) {
+                    ForEach(0..<columnsCount, id: \.self) { column in
+                        Square(xPosition: row, yPosition: column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [Color("MercuryLime"), .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .ignoresSafeArea()
+                VStack(alignment: .trailing) {
+                    GameGrid()
+                }
+                .navigationBarTitle("Conway's Game of Life", displayMode: .large)
+            }
+        }
     }
 }
 
